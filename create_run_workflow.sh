0332afd962677b225fd107988b7bcab37154cb05
soolee <duplexa@gmail.com>
2016-09-19 15:31:06 -0400

    modified script url from dev to master

--- create_run_workflow.sh
+++ create_run_workflow.sh
@@ -3,7 +3,7 @@ JOBID=$1
 USERDATA_DIR=$2
 [!-d $USERDATA_DIR] || mkdir -p $USERDATA_DIR
 RUN_WORKFLOW_FILE=$USERDATA_DIR/run_workflow.$JOBID.sh
-SCRIPT_URL=https://raw.githubusercontent.com/hms-dbmi/tibanna/dev/
+SCRIPT_URL=https://raw.githubusercontent.com/hms-dbmi/tibanna/master/
 echo "#!/bin/bash" > $RUN_WORKFLOW_FILE
 echo "JOBID=$JOBID" >> $RUN_WORKFLOW_FILE
 echo "RUN_SCRIPT=aws_run_workflow.sh" >> $RUN_WORKFLOW_FILE
