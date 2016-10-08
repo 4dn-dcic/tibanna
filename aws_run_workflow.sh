5c292ebb2839f7462f7b07e061a1b69c26a0477a
soolee <duplexa@gmail.com>
2016-10-07 22:02:18 -0400

    fixed md5sum overwriting other jobs.

--- aws_run_workflow.sh
+++ aws_run_workflow.sh
@@ -10,7 +10,7 @@ LOCAL_OUTDIR=$EBS_DIR/out
 LOCAL_CWLDIR=$LOCAL_OUTDIR ## cwl-runner directory handling is so great that we had to do this..
 LOCAL_INPUT_DIR=$EBS_DIR/input  ## WARNING: also hardcoded in aws_decode_run_json.py
 LOCAL_REFERENCE_DIR=$EBS_DIR/reference  ## WARNING: also hardcoded in aws_decode_run_json.py
-MD5FILE=md5sum.txt
+MD5FILE=$JOBID.md5sum.txt
 SCRIPTS_URL=https://raw.githubusercontent.com/hms-dbmi/tibanna/master/
 INPUT_YML_FILE=inputs.yml
 DOWNLOAD_COMMAND_FILE=download_command_list.txt
@@ -22,6 +22,7 @@ ERRFILE=$LOCAL_OUTDIR/$JOBID.error  # if this is found on s3, that means somethi
 
 # function that executes a command and collecting log
 exl(){ $@ >> $LOGFILE 2>> $LOGFILE; ERRCODE=$?; STATUS+=,$ERRCODE; if [ "$ERRCODE" -ne 0 -a ! -z "$OUTBUCKET" ]; then send_error; fi; } ## usage: exl command  ## ERRCODE has the error code for the command. if something is wrong and if OUTBUCKET has already been defined, send error to s3.
+exle(){ $@ >> /dev/null 2>> $LOGFILE; ERRCODE=$?; STATUS+=,$ERRCODE; if [ "$ERRCODE" -ne 0 -a ! -z "$OUTBUCKET" ]; then send_error; fi; } ## usage: exl command  ## ERRCODE has the error code for the command. if something is wrong and if OUTBUCKET has already been defined, send error to s3. This one eats stdout. Useful for downloading/uploading files to/from s3, because it writes progress to stdout.
 
 # function that sends log to s3 (it requires OUTBUCKET to be defined, which is done by sourcing $ENV_FILE.)
 send_log(){  aws s3 cp $LOGFILE s3://$OUTBUCKET; }  ## usage: send_log (no argument)
@@ -79,7 +80,7 @@ done
 ### download data & reference files from s3
 exl cat $DOWNLOAD_COMMAND_FILE
 exl date 
-exl source $DOWNLOAD_COMMAND_FILE 
+exle source $DOWNLOAD_COMMAND_FILE 
 exl date
 exl ls
 send_log 
@@ -114,7 +115,7 @@ mv $MD5FILE $LOCAL_OUTDIR
 exl date ## done time
 send_log
 exl ls -lhtr $LOCAL_OUTDIR/
-exl aws s3 cp --recursive $LOCAL_OUTDIR s3://$OUTBUCKET
+exle aws s3 cp --recursive $LOCAL_OUTDIR s3://$OUTBUCKET
 send_log
  
 ### updating status
@@ -123,7 +124,7 @@ if [ `echo $STATUS| sed 's/0//g' | sed 's/,//g'` ]; then export JOB_STATUS=$STAT
 # This env variable (JOB_STATUS) will be read by aws_update_run_json.py and the result will go into $POSTRUN_JSON_FILE_NAME. 
 ### 8. create a postrun.json file that contains the information in the run.json file and additional information (status, stop_time)
 exl ./aws_update_run_json.py $RUN_JSON_FILE_NAME $POSTRUN_JSON_FILE_NAME
-exl aws s3 cp $POSTRUN_JSON_FILE_NAME s3://$OUTBUCKET/$POSTRUN_JSON_FILE_NAME
+exle aws s3 cp $POSTRUN_JSON_FILE_NAME s3://$OUTBUCKET/$POSTRUN_JSON_FILE_NAME
 if [ ! -z $JOB_STATUS -a $JOB_STATUS == 0 ]; then touch $JOBID.success; aws s3 cp $JOBID.success s3://$OUTBUCKET/; fi
 send_log
 
