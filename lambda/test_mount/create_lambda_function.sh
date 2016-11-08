4a2b30b2a7328fb6884d23e00f7b1ed35dd4443b
soolee <duplexa@gmail.com>
2016-11-08 14:00:50 -0500

    minor changes

---
+++ lambda/test_mount/create_lambda_function.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+name=$1
+zipfilepath=$2
+desc=$name
+role_arn='arn:aws:iam::643366669028:role/service-role/readS3'
+source ~/bin/awscli/bin/activate
+aws lambda create-function  \
+    --region us-east-1          \
+    --runtime python2.7       \
+    --role $role_arn  \
+    --description "$desc" \
+    --timeout 60      \
+    --memory-size 128 \
+    --handler lambda_function.lambda_handler \
+    --zip-file fileb://$zipfilepath  \
+    --function-name $name
+
