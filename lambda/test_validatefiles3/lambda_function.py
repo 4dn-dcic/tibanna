be616d49cae9350229493d34a318a6524573f6e5
SooLee <duplexa@gmail.com>
2016-11-30 23:55:27 -0500

    multi-volume handling. code structure changed to handle this. works now.

--- lambda/test_validatefiles3/lambda_function.py
+++ lambda/test_validatefiles3/lambda_function.py
@@ -8,13 +8,26 @@ import time
 import sys
 import random
 
-sbg_base_url = "https://api.sbgenomics.com/v2"
 sbg_project_id = "4dn-dcic/dev"
 bucket_for_token = "4dn-dcic-sbg" # an object containing a sbg token is in this bucket 
 object_for_token = "token-4dn-labor" # an object containing a sbg token
-volume_name = '4dn_s3' # name of the volume to be mounted on sbg.
-volume_id = '4dn-labor/' + volume_name # ID of the volume to be mounted on sbg.
 object_for_access_key = 's3-access-key-4dn-labor'  # an object containing security credentials for a user 'sbg_s3', who has access to 4dn s3 buckets. It's in the same bucket_for_token. Public_key\nsecret_key. We need this not for the purpose of lambda connecting to our s3, but SBG requires it for mounting our s3 bucket to the sbg s3.
+
+
+class SBGVolume:
+  prefix = '4dn_s3'
+  account = '4dn-labor'
+
+  def __init__(self, volume_suffix=None):
+
+    if volume_suffix is None:
+      volume_suffix=''
+      for i in xrange(8):
+        r=random.choice('abcdefghijklmnopqrstuvwxyz1234567890')
+        volume_suffix += r
+  
+    self.name = self.prefix + volume_suffix # name of the volume to be mounted on sbg.
+    self.id = self.account + '/' + self.name  # ID of the volume to be mounted on sbg.
 
 
 
@@ -49,6 +62,185 @@ class SBGTaskInput(object):
       return(False)
 
 
+
+class SBGWorkflowRun(object): ## one object per workflow run
+
+  base_url = "https://api.sbgenomics.com/v2"
+
+  def __init__(self, token, project_id):
+    self.token=token
+    self.header={ "X-SBG-Auth-Token" : token, "Content-type" : "application/json" }
+    self.project_id=project_id
+    self.volume_list=[]  ## list of volumes mounted for the current run. We keep the information here so that they can be deleted later
+    self.import_id_list=[]  ## list of import ids for the files imported for the current run.
+    self.task_id=None  ## task_id for the current workflow run. It will be assigned after draft task is successfully created. We keep the information here, so we can re-run the task if it fails and also for the sanity check - so that we only run tasks that we created.
+    self.task_input=None  ## SBGTaskInput object
+
+
+  ## function that creates volume
+  ## sbg_volume: SBGVolume object
+  ## bucket_name: name of bucket to mount
+  ## public_key, secret_key: keys for S3 bucket
+  ## bucket_object_prefix : for subdirectory inside the bucket, use subdirectory_name+'/'
+  ## access_mode : 'ro' for readonly 'rw' for read and write
+  def create_volumes (self, sbg_volume, bucket_name, public_key, secret_key, bucket_object_prefix='', access_mode='rw'):
+    volume_url = self.base_url + "/storage/volumes/"
+    data = {
+      "name" : sbg_volume.name,
+      "description" : "some volume" ,
+      "service" : {
+         "type": "s3",
+         "bucket": bucket_name,
+         "prefix": bucket_object_prefix, ## prefix of objects, this causs some confusion later when referring to the mounted file, because you have to exclude the prefix part, so just keep it as ''. 
+         "credentials": {
+           "access_key_id": public_key, ## public access key for our s3 bucket
+           "secret_access_key": secret_key  ## secret access key for our s3 bucket
+         },
+         "properties": {
+           "sse_algorithm": "AES256"
+         }
+      },
+      "access_mode" : access_mode  ## either 'rw' or 'ro'.
+    }
+
+    try:
+      response = requests.post(volume_url, headers=self.header, data=json.dumps(data))
+      ## update volume_list 
+      if sbg_volume not in self.volume_list:
+        self.volume_list.append(sbg_volume)
+      return(response.json())
+    except Exception as e:
+      print(e)
+      print("volume creation error")
+      raise e
+
+
+  ## function that initiations importing (mounting) an object on 4dn s3 to SBG s3
+  ## source_filename : object key on 4dn s3
+  ## dest_filename : filename-to-be on SBG s3 (default, it is set to be the same as source_filename) 
+  ## return value : the newly imported (mounted) file's ID on SBG S3
+  def import_volume_content (self, sbg_volume, object_key, dest_filename=None):
+ 
+    if sbg_volume not in self.volume_list:
+      print("Error: the specified volume doesn't exist in the current workflow run.")  
+      sys.exit()
+
+    source_filename = object_key
+    if dest_filename is None:
+       dest_filename = object_key
+    import_url = self.base_url + "/storage/imports"
+    data = {
+      "source":{
+        "volume": sbg_volume.id,
+        "location": source_filename
+      },
+      "destination": {
+        "project": self.project_id,
+        "name": dest_filename
+      },
+      "overwrite": False
+    }
+    response = requests.post(import_url, headers=self.header, data=json.dumps(data))
+
+    if response.json().has_key('id'):
+      import_id = response.json().get('id')
+      if import_id not in self.import_id_list:
+        self.import_id_list.append(import_id)
+      return(import_id)
+    else:
+      print("Error: import not successful.")
+      sys.exit()  
+
+
+  ## check the status and other details of import
+  def get_details_of_import (self, import_id):
+
+    if import_id not in self.import_id_list:
+      print("Error: the import id is not in the SBG workflow run.")
+      sys.exit()
+
+    import_url = self.base_url + "/storage/imports/" + import_id
+    data = { "import_id" : import_id }
+  
+    ## wait while import is pending
+    while True:
+      response = requests.get(import_url, headers=self.header, data=json.dumps(data))
+      if response.json().get('state') != 'PENDING':
+        break;
+      time.sleep(2)
+  
+    ## if import failed 
+    if response.json().get('state') != 'COMPLETED':
+      print(response.json())
+      sys.exit() 
+  
+    return(response.json())
+
+
+  ## create a draft task on SBG, given a SBGTaskInput object  
+  def create_task(self, sbg_task_input):
+      url = self.base_url + "/tasks"
+      data = sbg_task_input.__dict__
+      resp = requests.post(url, headers=self.header, data=json.dumps(data))
+
+      if resp.json().has_key('id'):
+        self.task_id = resp.json().get('id')
+        self.task_input = sbg_task_input
+        return(resp.json()) 
+      else:
+        print(resp.json())
+        sys.exit()
+  
+
+  ## run task on SBG
+  ## A draft task must be created before running it
+  def run_task (self):
+
+      if self.task_id is None:
+        print("Error: no task_id available. Create a draft task first.")
+        sys.exit()
+
+      url = self.base_url + "/tasks/" + self.task_id + "/actions/run"
+      data = self.task_input.__dict__
+      resp = requests.post(url, headers=self.header, data=json.dumps(data))
+      return(resp.json()) ## return the run_task response
+  
+  
+  ## check status of task
+  def check_task (self):
+      if self.task_id is None:
+        print("Error: no task_id available. Create a draft task first.")
+        sys.exit()
+
+      url = self.base_url + "/tasks/" + self.task_id
+      data = {}
+      response = requests.get(url, headers=self.header, data=json.dumps(data))
+      return ( response.json() )
+
+
+  ## example method for creating sbgtaskinput for validate app given the response body of import request in json format
+  def create_data_payload_validatefiles ( import_response ):
+  
+    try:
+  
+       file_id = import_response.get('result').get('id') # imported Id on SBG
+       file_name = import_response.get('result').get('name') # imported file name on SBG
+  
+       app_name = "validate"  
+  
+       sbgtaskinput = SBGTaskInput(self.project_id, app_name)
+       sbgtaskinput.add_inputfile(file_name, file_id, "input_file")
+       sbgtaskinput.add_inputparam("fastq","type")
+  
+       return(sbgtaskinput)
+  
+    except Exception as e:
+       print(e)
+       print('Error creating a task payload')
+       raise e 
+
+
+
 ## function that grabs SBG token from a designated S3 bucket
 def get_sbg_token (s3):
   try:
@@ -80,145 +272,6 @@ def format_response (response):
   return json.dumps(json.loads(response.text), indent=4)
 
 
-## function that creates volume
-def create_volumes (token, volume_name, bucket_name, public_key, secret_key, bucket_object_prefix='', access_mode='rw'):
-  volume_url = sbg_base_url + "/storage/volumes/"
-  header= { "X-SBG-Auth-Token" : token, "Content-type" : "application/json" }
-  data = {
-    "name" : volume_name,
-    "description" : "some volume" ,
-    "service" : {
-       "type": "s3",
-       "bucket": bucket_name,
-       "prefix": bucket_object_prefix, ## prefix of objects, this causs some confusion later when referring to the mounted file, because you have to exclude the prefix part, so just keep it as ''. 
-       "credentials": {
-         "access_key_id": public_key, ## public access key for our s3 bucket
-         "secret_access_key": secret_key  ## secret access key for our s3 bucket
-       },
-       "properties": {
-         "sse_algorithm": "AES256"
-       }
-    },
-    "access_mode" : access_mode  ## either 'rw' or 'ro'.
-  }
-  response = requests.post(volume_url, headers=header, data=json.dumps(data))
-  return(format_response(response))
-
-
-
-
-## function that initiations importing (mounting) an object on 4dn s3 to SBG s3
-## token : SBG authentication token
-## volume_id : the volume-to-be on SBG s3, e.g. duplexa/myvolume3 (it looks like the first part (duplexa) should match the ownder of the token.)
-## source_filename : object key on 4dn s3
-## dest_filename : filename-to-be on SBG s3 (default, it is set to be the same as source_filename) 
-## return value : the newly imported (mounted) file's ID on SBG S3
-def import_volume_content (token, volume_id, object_key, dest_filename=None):
-
-  source_filename = object_key
-  if dest_filename is None:
-     dest_filename = object_key
-  import_url = sbg_base_url + "/storage/imports"
-  header= { "X-SBG-Auth-Token" : token, "Content-type" : "application/json" }
-  data = {
-    "source":{
-      "volume": volume_id,
-      "location": source_filename
-    },
-    "destination": {
-      "project": sbg_project_id,
-      "name": dest_filename
-    },
-    "overwrite": False
-  }
-  response = requests.post(import_url, headers=header, data=json.dumps(data))
-  return(response.json().get('id'))
-
-
-
-def get_details_of_import (token, import_id):
-  import_url = sbg_base_url + "/storage/imports/" + import_id
-  header= { "X-SBG-Auth-Token" : token, "Content-type" : "application/json" }
-  data = { "import_id" : import_id }
-
-  ## wait while import is pending
-  while True:
-    response = requests.get(import_url, headers=header, data=json.dumps(data))
-    if response.json().get('state') != 'PENDING':
-      break;
-    time.sleep(2)
-
-  ## if import failed 
-  if response.json().get('state') != 'COMPLETED':
-    print(response.json())
-    sys.exit() 
-
-  return(response.json())
-
-
-def create_data_payload_validatefiles ( import_response ):
-
-  try:
-
-     file_id = import_response.get('result').get('id') # imported Id on SBG
-     file_name = import_response.get('result').get('name') # imported file name on SBG
-
-     app_name = "validate"  
-
-     sbgtaskinput = SBGTaskInput(sbg_project_id, app_name)
-     sbgtaskinput.add_inputfile(file_name, file_id, "input_file")
-     sbgtaskinput.add_inputparam("fastq","type")
-     data = sbgtaskinput.__dict__
-
-     return(data)
-
-  except Exception as e:
-     print(e)
-     print('Error creating a task payload')
-     raise e 
-
-
-def create_task(token, data):
-    url = sbg_base_url + "/tasks"
-
-    headers = {'X-SBG-Auth-Token': token,
-               'Content-Type': 'application/json'}
-
-    resp = requests.post(url, headers=headers, data=json.dumps(data))
-    print(resp.json())
-
-    if resp.json().has_key('id'):
-      return(resp.json()) 
-    else:
-      print(resp.json())
-      sys.exit()
-
-
-def run_task (token, create_task_response):
-    task_id = create_task_response.get('id')
-    url = sbg_base_url + "/tasks/" + task_id + "/actions/run"
-    headers = {'X-SBG-Auth-Token': token,
-               'Content-Type': 'application/json'}
-
-    data = create_task_response.get('inputs')
-    resp = requests.post(url, headers=headers, data=json.dumps(data))
-    return(resp.json()) ## return the run_task response
-
-
-## This function is TEMPORARY - most of the jobs will run for more than the lifespan of this lambda function.
-## Use it for test purpose for a tiny input file.
-def check_task (token, task_id):
-    url = sbg_base_url + "/tasks/" + task_id
-    headers = {'X-SBG-Auth-Token': token,
-               'Content-Type': 'application/json'}
-
-    data = {}
-    resp = requests.get(url, headers=headers, data=json.dumps(data))
-
-    ## wait while task is pending
-    response = requests.get(url, headers=headers, data=json.dumps(data))
-    return ( response.json() )
-
 
 
 def lambda_handler(event, context):
@@ -249,6 +302,8 @@ def lambda_handler(event, context):
         print('Error getting token and access key from bucket {}.'.format(bucket))
         raise e
 
+    ## create a sbg workflow run object to use
+    sbg = SBGWorkflowRun(token,sbg_project_id)
 
     ## mount multiple input files to SBG S3 and
     ## create a SBGTaskInput object that contains multiple files and given parameters
@@ -269,9 +324,13 @@ def lambda_handler(event, context):
     
         ## mount the bucket and import the file
         try: 
-            sbg_create_volume_response = create_volumes (token, volume_name, bucket, public_key=access_key[0], secret_key=access_key[1])
-            sbg_import_id = import_volume_content (token, volume_id, key)
-            sbg_check_import_response = get_details_of_import(token, sbg_import_id)
+            sbg_volume = SBGVolume()
+            sbg_create_volume_response = sbg.create_volumes (sbg_volume, bucket, public_key=access_key[0], secret_key=access_key[1])
+            print(sbg_create_volume_response)  ## DEBUGGING
+            sbg_import_id = sbg.import_volume_content (sbg_volume, key)
+            print(sbg_import_id)
+            sbg_check_import_response = sbg.get_details_of_import(sbg_import_id)
+            print(sbg_check_import_response)
             
         except Exception as e:
             print(e)
@@ -292,10 +351,9 @@ def lambda_handler(event, context):
 
     # run a validatefiles task 
     try:
-        #task_data = create_data_payload_validatefiles ( sbg_check_import_response)
-        task_data = task_input.__dict__
-        create_task_response = create_task ( token, task_data )
-        run_response = run_task (token, create_task_response)
+        #task_data = sbg.create_data_payload_validatefiles( sbg_check_import_response)
+        create_task_response = sbg.create_task( task_input )
+        run_response = sbg.run_task()
 
     except Exception as e:
         print(e)
@@ -305,8 +363,7 @@ def lambda_handler(event, context):
 
     # check task
     try:
-        task_id = create_task_response.get('id')  
-        check_task_response = check_task (token, task_id)
+        check_task_response = sbg.check_task()
         return( check_task_response )
 
     except Exception as e:
@@ -314,6 +371,14 @@ def lambda_handler(event, context):
         print('Error running a task')
         raise e
     
+    # for debugging
+    try:
+        print(json.dumps(sbg.__dict__))
+    except Exception as e:
+        print(e)
+        print("Error printing the SBGWorkflowRun object.")
+        raise e
+
 
 
 if __name__ == "__main__":
