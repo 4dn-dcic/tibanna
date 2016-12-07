56bbd9408d1efd9e46053218ae6a6816fb082e50
SooLee <duplexa@gmail.com>
2016-12-07 16:47:00 -0500

    moved awsub, awstat, etc to directory awsf

--- userdata/run_workflow.UXN9jBFbYznk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JOBID=UXN9jBFbYznk
-RUN_SCRIPT=aws_run_workflow.sh
-SCRIPT_URL=https://raw.githubusercontent.com/hms-dbmi/tibanna/master/
-wget $SCRIPT_URL/$RUN_SCRIPT
-chmod +x $RUN_SCRIPT
-source $RUN_SCRIPT $JOBID
--- userdata/run_workflow.rv44vwT7YGbg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JOBID=rv44vwT7YGbg
-RUN_SCRIPT=aws_run_workflow.sh
-SCRIPT_URL=https://raw.githubusercontent.com/hms-dbmi/tibanna/master/
-wget $SCRIPT_URL/$RUN_SCRIPT
-chmod +x $RUN_SCRIPT
-source $RUN_SCRIPT $JOBID
