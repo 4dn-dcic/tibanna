c22d55d118903eb2913c1b0c5de8ef14bf52b607
SooLee <duplexa@gmail.com>
2017-02-28 17:27:58 +0000

    cleaned up code

--- core/check_export_sbg/service.py
+++ core/check_export_sbg/service.py
@@ -51,12 +51,15 @@ def fastqc_updater(status, sbg, ff_meta):
     try:
         original_file = utils.get_metadata(accession, key=ff_key)
     except Exception as e:
-        raise Exception("Couldn't get metadata for accession {} : original_file ={}\n".format(accession, str(original_file)) % e)
+        raise Exception("Couldn't get metadata for accession {} : " \
+                        + "original_file ={}\n".format(accession, str(original_file)) % e)
     patch_file = {'quality_metric': qc_meta['@id']}
     try:
         utils.patch_metadata(patch_file, original_file['uuid'], key=ff_key)
     except Exception as e:
-        raise Exception("patch_metadata failed in fastqc_updater. %s. original_file ={}\n".format(str(original_file)) % e)
+        raise Exception("patch_metadata failed in fastqc_updater. %s. " \
+                        + "original_file ={}\n".format(str(original_file)) % e)
+
 
 def md5_updater(status, sbg, ff_meta):
     # get metadata about original input file
