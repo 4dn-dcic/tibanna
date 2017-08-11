4102b8f803ef65400ffb630b6335bae0399aaf56
SooLee <duplexa@gmail.com>
2017-08-11 20:49:17 +0000

    updated aws_run_workflow for cwl tmp directory

--- awsf/aws_run_workflow.sh
+++ awsf/aws_run_workflow.sh
@@ -109,7 +109,7 @@ exl source /home/ec2-user/venv/cwl/bin/activate
 cwd0=$(pwd)
 cd $LOCAL_OUTDIR  ## so that other downstream cwl files can be accessed and so that the output files can be captured.
 mkdir -p $LOCAL_CWL_TMPDIR
-exl cwl-runner --tmpdir-prefix $LOCAL_CWL_TMPDIR $LOCAL_CWLDIR/$MAIN_CWL $cwd0/$INPUT_YML_FILE
+exl cwl-runner --outdir $LOCAL_OUTDIR --tmp-outdir-prefix $LOCAL_CWL_TMPDIR --tmpdir-prefix $LOCAL_CWL_TMPDIR $LOCAL_CWLDIR/$MAIN_CWL $cwd0/$INPUT_YML_FILE
 deactivate
 cd $cwd0
 send_log 
