f6af99d4f473c1889dd1f76a72cd9d4fd4c4e787
SooLee <duplexa@gmail.com>
2017-09-11 02:33:42 +0000

    cleanup

--- core/ff_utils.py
+++ core/ff_utils.py
@@ -176,7 +176,7 @@ class WorkflowRunMetadata(object):
 
 class ProcessedFileMetadata(object):
     def __init__(self, uuid=None, accession=None, file_format='', lab='4dn-dcic-lab',
-                 extra_files = [],
+                 extra_files=[],
                  award='1U01CA200059-01', status='to be uploaded by workflow'):
         self.uuid = uuid if uuid else str(uuid4())
         self.accession = accession if accession else generate_rand_accession()
