8f44381b28f61d5501cde419274cf8c2b9de1916
SooLee <duplexa@gmail.com>
2017-09-11 01:27:54 +0000

    type fix for aws_upload_output py

--- awsf/aws_upload_output_update_json.py
+++ awsf/aws_upload_output_update_json.py
@@ -47,23 +47,22 @@ for k in cwl_output:
         raise Exception("cannot update target info to json %s" % e)
 
     if 'secondaryFiles' in cwl_output[k]:
-        sf = cwl_output[k]['secondaryFiles'][0]
-        for kk in sf:
-            source = sf[kk].get('path')
-            source_name = source.replace(source_directory, '')
-            if k in secondary_output_target:
-                target = secondary_output_target[k]  # change file name to what's specified in secondary_output_target
-            else:
-                target = source_name  # do not change file name
-            try:
-                print("uploading output file {} upload to {}".format(source, output_bucket + '/' + target))
-                s3.upload_file(source, output_bucket, target)
-            except Exception as e:
-                raise Exception("output file {} upload to {} failed. %s".format(source, output_bucket + '/' + target) % e )
-            try:
-                sf[kk]['target'] = target
-            except Exception as e:
-                raise Exception("cannot update target info to json %s" % e)
+        sf = cwl_output[k]['secondaryFiles'][0]  # only the first secondary file
+        source = sf.get('path')
+        source_name = source.replace(source_directory, '')
+        if k in secondary_output_target:
+            target = secondary_output_target[k]  # change file name to what's specified in secondary_output_target
+        else:
+            target = source_name  # do not change file name
+        try:
+            print("uploading output file {} upload to {}".format(source, output_bucket + '/' + target))
+            s3.upload_file(source, output_bucket, target)
+        except Exception as e:
+            raise Exception("output file {} upload to {} failed. %s".format(source, output_bucket + '/' + target) % e )
+        try:
+            sf['target'] = target
+        except Exception as e:
+            raise Exception("cannot update target info to json %s" % e)
 
 ## write to new json file
 with open(json_new, 'w') as json_new_f:
