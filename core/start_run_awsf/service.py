c53c323393a9d6c489a0c195e34a8550979182a6
j1z0 <jeremy@softworks.com.my>
2018-02-01 20:53:33 -0500

    spelling mistake

--- core/start_run_awsf/service.py
+++ core/start_run_awsf/service.py
@@ -126,7 +126,7 @@ def real_handler(event, context):
     ff_meta = ff_utils.create_ffmeta_awsem(workflow_uuid, app_name, input_files, tag=tag,
                                            run_url=tibanna.settings.get('url', ''),
                                            output_files=output_files, parameters=parameters,
-                                           alias=event.get('wfr_alisa'))
+                                           alias=event.get('wfr_alias'))
 
     LOG.info("ff_meta is %s" % ff_meta.__dict__)
 
