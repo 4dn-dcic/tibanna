64343647a202c15af6a9fb3ced73ed5e8801d254
SooLee <duplexa@gmail.com>
2018-02-07 18:19:38 +0000

    now postrunjson attached as url

--- tests/core/update_ffmeta_awsem/test_service.py
+++ tests/core/update_ffmeta_awsem/test_service.py
@@ -8,11 +8,9 @@ import uuid
 
 @valid_env
 @pytest.mark.webtest
-def test_add_postrunjson_to_ff_meta(update_ffmeta_event_data, tibanna_env, ff_keys):
-    update_ffmeta_event_data.update(tibanna_env)
-    att_uuid = add_postrunjson_to_ff_meta(update_ffmeta_event_data, ff_keys)
-    assert att_uuid
-    assert len(att_uuid) == len(str(uuid.uuid4()))
+def test_get_postrunjson_url(update_ffmeta_event_data):
+    url = get_postrunjson_url(update_ffmeta_event_data)
+    assert url == 'https://s3.amazonaws.com/tibanna-output/8fRIlIfwRNDT.postrun.json'
 
 
 @valid_env
@@ -21,8 +19,8 @@ def test_update_ffmeta_awsem_e2e(update_ffmeta_event_data, tibanna_env):
     update_ffmeta_event_data.update(tibanna_env)
     ret = handler(update_ffmeta_event_data, None)
     assert json.dumps(ret)
-    assert 'documents' in ret['ff_meta']
-    assert ret['ff_meta']['documents']
+    assert 'awsem_postrun_json' in ret['ff_meta']
+    assert ret['ff_meta']['awsem_postrun_json'] == 'https://s3.amazonaws.com/tibanna-output/8fRIlIfwRNDT.postrun.json'
     # test that file is uploaded?
 
 '''
