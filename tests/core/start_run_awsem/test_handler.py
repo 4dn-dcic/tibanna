c67688c0109d93f44971e96c8a92654fe5d67b04
SooLee <duplexa@gmail.com>
2018-02-12 21:52:50 +0000

    source experiment/input file handling tests

--- tests/core/start_run_awsem/test_handler.py
+++ tests/core/start_run_awsem/test_handler.py
@@ -4,6 +4,8 @@ from core.start_run_awsem.service import (
     get_format_extension_map,
     handle_processed_files,
     proc_file_for_arg_name,
+    process_input_file_info,
+    get_source_experiment,
 )
 from ..conftest import valid_env
 from core.utils import Tibanna
@@ -181,7 +183,7 @@ def test_process_input_file_info(run_awsem_event_data):
         "uuid": ["d2c897ec-bdb2-47ce-b1b1-845daccaa571","d2c897ec-bdb2-47ce-b1b1-845daccaa571"],
         "object_key": ["4DNFI25JXLLI.pairs.gz","4DNFI25JXLLI.pairs.gz"]
     }
-    args = dict()
+    args = {'input_files': {}}
     data = run_awsem_event_data
     tibanna_settings = data.get('_tibanna', {})
     # if they don't pass in env guess it from output_bucket
@@ -194,3 +196,25 @@ def test_process_input_file_info(run_awsem_event_data):
     assert 'secondary_files' in args
 
 
+@valid_env
+@pytest.mark.webtest
+def test_get_source_experiment(run_awsem_event_data):
+    input_file = {
+        "bucket_name": "elasticbeanstalk-fourfront-webdev-wfoutput",
+        "workflow_argument_name": "input_pairs",
+        "uuid": ["d2c897ec-bdb2-47ce-b1b1-845daccaa571","d2c897ec-bdb2-47ce-b1b1-845daccaa571"],
+        "object_key": ["4DNFI25JXLLI.pairs.gz","4DNFI25JXLLI.pairs.gz"]
+    }
+    data = run_awsem_event_data
+    tibanna_settings = data.get('_tibanna', {})
+    # if they don't pass in env guess it from output_bucket
+    env = tibanna_settings.get('env')
+    # tibanna provides access to keys based on env and stuff like that
+    tibanna = Tibanna(env, s3_keys=data.get('s3_keys'),
+                      ff_keys=data.get('ff_keys'),
+                      settings=tibanna_settings)
+    res = get_source_experiment(input_file, tibanna.ff_keys)
+    res0 = ff_utils.get_metadata(input_file['uuid'][0], tibanna.ff_keys)
+    print(res0)
+    assert 'source_experiments' in res0
+    assert 'fake_source_experiment' in res
