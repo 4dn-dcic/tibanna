596f329cd58acbe78823d8e7eef91b866f4b5c65
SooLee <duplexa@gmail.com>
2018-02-13 22:03:55 -0500

    wf-based filter to launch_utils

--- core/launch_utils.py
+++ core/launch_utils.py
@@ -7,8 +7,8 @@ import time
 import os
 
 
-def prep_awsem_template(filename, webprod=False, tag=None,
-                        Tibanna_dir=os.path.dirname(os.path.realpath(__file__))):
+def prep_awsem_template(filename, webprod=False, tag=None):
+    Tibanna_dir=os.path.dirname(os.path.realpath(__file__)).replace('/core', '')
     template = Tibanna_dir + '/test_json/' + filename
     with open(template, 'r') as f:
         awsem_template = json.load(f)
@@ -98,29 +98,36 @@ def get_connection(keypairs_file):
     return(connection)
 
 
-def prep_input_file_entry_list_for_single_exp(prev_workflow_title, prev_output_argument_name, connection):
+def prep_input_file_entry_list_for_single_exp(prev_workflow_title, prev_output_argument_name, connection,
+                                              addon=None, wfuuid=None):
     schema_name = 'search/?type=WorkflowRunAwsem&workflow.title=' + prev_workflow_title + '&run_status=complete'
     response = fdnDCIC.get_FDN(schema_name, connection)
-    files_for_ep = map_exp_to_inputfile_entry(response, prev_output_argument_name, connection)
+    files_for_ep = map_exp_to_inputfile_entry(response, prev_output_argument_name, connection,
+                                              addon=addon, wfuuid=wfuuid)
     return(files_for_ep)
 
 
-def prep_input_file_entry_list_for_merging_expset(prev_workflow_title, prev_output_argument_name, connection):
+def prep_input_file_entry_list_for_merging_expset(prev_workflow_title, prev_output_argument_name, connection,
+                                                  addon=None, wfuuid=None):
     files_for_ep = prep_input_file_entry_list_for_single_exp(prev_workflow_title,
                                                              prev_output_argument_name,
-                                                             connection)
+                                                             connection, addon, wfuuid)
     ep_lists_per_eps = map_expset_to_allexp(files_for_ep.keys(), connection)
     input_files_list = map_expset_to_inputfile_list(ep_lists_per_eps, files_for_ep)
     return(input_files_list)
 
 
-def create_inputfile_entry(file_uuid, connection, addon=None):
+def create_inputfile_entry(file_uuid, connection, addon=None, wfr_input_filter=None):
     """create an input file entry (uuid, accession, object_key)
     addon : list of following strings (currently only 're' is available to add restriction enzyme info)
-    assumes file is a processed file
+    wfr_input_filter : workflow_uuid, return None if specified and has a completed or
+    started run of the specified workflow
+    assumes file is a processed file (has source_experiments field)
     assumes single source_experiments
     """
     file_dict = fdnDCIC.get_FDN(file_uuid, connection)
+    if 'source_experiments' not in file_dict:
+        return(None)
     sep = file_dict['source_experiments'][0]
     sep_dict = fdnDCIC.get_FDN(sep, connection)
     sep_id = sep_dict['@id']
@@ -130,10 +137,44 @@ def create_inputfile_entry(file_uuid, connection, addon=None):
     if addon:
         if 're' in addon:
             entry['RE'] = get_digestion_enzyme_for_expr(sep, connection)
+
+    if wfr_input_filter:
+        skip = False
+        wfr_info = get_info_on_workflowrun_as_input(file_dict, connection)
+        if wfr_input_filter in wfr_info:
+            if 'complete' in wfr_info[wfr_input_filter]:
+                skip = True
+            if 'started' in wfr_info[wfr_input_filter]:
+                skip = True
+        if skip:
+            return(None)
+
     return(entry)
 
 
-def map_exp_to_inputfile_entry(wfr_search_response, prev_output_argument_name, connection, addon=None):
+def get_info_on_workflowrun_as_input(file_dict, connection):
+    """given a json for file, returns a dictionary with workflow uuids as keys.
+    dictionary structure : dict{wf_uuid}{run_status} = [wfr_id1, wfr_id2, ... ]
+    These workflow uuids are the the ones in the workflow runs
+    that has the given file as input
+    """
+    wfr_info = dict()
+    if 'workflow_run_inputs' in file_dict:
+        wfr_list = file_dict.get("workflow_run_inputs")
+        if wfr_list:
+            for wfr in wfr_list:
+                wfr_dict = fdnDCIC.get_FDN(wfr, connection)
+                wf = wfr_dict['workflow'].replace('/workflows/', '').replace('/', '')
+                run_status = wfr_dict['run_status']
+                if wf not in wfr_info:
+                   wfr_info[wf] = dict()
+                if run_status not in wfr_info[wf]:
+                    wfr_info[wf][run_status] = []
+                wfr_info[wf][run_status].append(wfr)
+    return(wfr_info)
+
+
+def map_exp_to_inputfile_entry(wfr_search_response, prev_output_argument_name, connection, addon=None, wfuuid=None):
     """single-experiment (id not uuid) -> one output file entry (uuid, accession, object_key)
     addon : list of following strings (currently only 're' is available to add restriction enzyme info)
     """
@@ -143,9 +184,12 @@ def map_exp_to_inputfile_entry(wfr_search_response, prev_output_argument_name, c
             if of['workflow_argument_name'] == prev_output_argument_name:
                 file_uuid = of['value']
                 break
-        file_entry = create_inputfile_entry(file_uuid, connection, addon=addon)
-        sep_id = file_entry['source_experiments'][0]
-        files_for_ep[sep_id] = file_entry
+        print(file_uuid)
+        file_entry = create_inputfile_entry(file_uuid, connection, addon=addon, wfr_input_filter=wfuuid)
+        if file_entry:
+            if 'source_experiments' in file_entry and file_entry['source_experiments']:
+                sep_id = file_entry['source_experiments'][0]
+                files_for_ep[sep_id] = file_entry
     return(files_for_ep)
 
 
@@ -224,6 +268,7 @@ def create_awsem_json_for_workflowrun(input_entry, awsem_template_file, workflow
 def collect_pairs_files_to_run_hi_c_processing_pairs(
         keypairs_file,
         webprod=True,
+        wfuuid='c9e0e6f7-b0ed-4a42-9466-cadc2dd84df0',
         prev_workflow_title='Hi-C%20Post-alignment%20Processing',
         prev_output_argument_name='filtered_pairs',
         awsem_template_json='awsem_hicpairs_easy.json',
@@ -239,20 +284,25 @@ def collect_pairs_files_to_run_hi_c_processing_pairs(
     connection = get_connection(keypairs_file)
     input_files_list = prep_input_file_entry_list_for_merging_expset(prev_workflow_title,
                                                                      prev_output_argument_name,
-                                                                     connection)
-    for entry in input_files_list:
-        awsem_json = create_awsem_json_for_workflowrun(entry, awsem_template_json, input_argument_name,
-                                                       awsem_tag=awsem_tag,
-                                                       parameters_to_override=parameters_to_override,
-                                                       parameters_to_delete=parameters_to_delete,
-                                                       webprod=webprod)
-        resp = _run_workflow(awsem_json, workflow=stepfunction_workflow)
-        print(resp)
+                                                                     connection,
+                                                                     wfuuid=wfuuid)
+    if input_files_list: 
+        for _, entry in input_files_list.iteritems():
+            print(entry)
+            awsem_json = create_awsem_json_for_workflowrun(entry, awsem_template_json, input_argument_name,
+                                                           awsem_tag=awsem_tag,
+                                                           parameters_to_override=parameters_to_override,
+                                                           parameters_to_delete=parameters_to_delete,
+                                                           webprod=webprod)
+            resp = _run_workflow(awsem_json, workflow=stepfunction_workflow)
+            print(resp)
+    return({'input_files_list': input_files_list})
 
 
 def collect_pairs_files_to_run_pairsqc(
         keypairs_file,
         webprod=True,
+        wfuuid='ae3a87cb-3fa2-469e-97c7-540fc2d0a117',
         prev_workflow_title='Hi-C%20Post-alignment%20Processing',
         prev_output_argument_name='filtered_pairs',
         awsem_template_json='awsem_pairsqc.json',
@@ -268,13 +318,16 @@ def collect_pairs_files_to_run_pairsqc(
     connection = get_connection(keypairs_file)
     input_files_list = prep_input_file_entry_list_for_single_exp(prev_workflow_title,
                                                                  prev_output_argument_name,
-                                                                 connection)
-    for entry in input_files_list:
-        parameters_to_override = {'sample_name': entry['accession'], 'enzyme': re_cutter[entry['RE']]}
-        awsem_json = create_awsem_json_for_workflowrun(entry, awsem_template_json, input_argument_name,
-                                                       awsem_tag=awsem_tag,
-                                                       parameters_to_override=parameters_to_override,
-                                                       parameters_to_delete=parameters_to_delete,
-                                                       webprod=webprod)
-        resp = _run_workflow(awsem_json, workflow=stepfunction_workflow)
-        print(resp)
+                                                                 connection,
+                                                                 wfuuid=wfuuid)
+    if input_files_list:
+        for _, entry in input_files_list.iteritems():
+            parameters_to_override = {'sample_name': entry['accession'], 'enzyme': re_cutter[entry['RE']]}
+            awsem_json = create_awsem_json_for_workflowrun(entry, awsem_template_json, input_argument_name,
+                                                           awsem_tag=awsem_tag,
+                                                           parameters_to_override=parameters_to_override,
+                                                           parameters_to_delete=parameters_to_delete,
+                                                           webprod=webprod)
+            resp = _run_workflow(awsem_json, workflow=stepfunction_workflow)
+            print(resp)
+    return({'input_files_list': input_files_list})
