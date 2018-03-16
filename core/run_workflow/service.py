d1a3df76a15cba4c0d340be205aa493c9a5e8ef9
j1z0 <jeremy@softworks.com.my>
2018-03-16 10:39:47 -0400

    run_workflow seems to have lost some code

--- core/run_workflow/service.py
+++ core/run_workflow/service.py
@@ -7,6 +7,6 @@ def handler(event, context):
     env_name = event['env_name']
     workflow = event.get('step_function_name')
     if workflow:
-        utils.run_workflow(event, env=env_name, workflow=workflow)
+        return utils.run_workflow(event, env=env_name, workflow=workflow)
     else:
-        utils.run_workflow(event, env=env_name)
+        return utils.run_workflow(event, env=env_name)
