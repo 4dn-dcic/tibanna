344bdb9339a4d85507ea9829d4ccdd44a8435fe2
SooLee <duplexa@gmail.com>
2018-04-30 19:58:08 +0000

    log->LOG to avoid gitignored

--- tests/awsf/test.sh
+++ tests/awsf/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ## testing output upload / postrun json creation
-python awsf/aws_upload_output_update_json.py tests/awsf/bqLd8oa7Tdzq.run.json tests/awsf/bqLd8oa7Tdzq.log.json tests/awsf/bqLd8oa7Tdzq.log tests/awsf/bqLd8oa7Tdzq.md5sum.txt tests/awsf/bqLd8oa7Tdzq.postrun.json.out
+python awsf/aws_upload_output_update_json.py tests/awsf/bqLd8oa7Tdzq.run.json tests/awsf/bqLd8oa7Tdzq.log.json tests/awsf/bqLd8oa7Tdzq.LOG tests/awsf/bqLd8oa7Tdzq.md5sum.txt tests/awsf/bqLd8oa7Tdzq.postrun.json.out
 
