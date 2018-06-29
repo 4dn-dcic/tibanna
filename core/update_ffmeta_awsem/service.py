6c778012a659c2ff5645123caa0b4e1013b874a2
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-06-29 14:30:20 -0400

    Back to higlass-generated uuids

--- core/update_ffmeta_awsem/service.py
+++ core/update_ffmeta_awsem/service.py
@@ -17,10 +17,8 @@ def donothing(status, sbg, ff_meta, ff_key=None):
 
 
 def register_to_higlass(tibanna, export_bucket, export_key, filetype, datatype):
-    file_id = export_key.split('/')[0]  # this should be file uuid
     payload = {"filepath": export_bucket + "/" + export_key,
-               "filetype": filetype, "datatype": datatype,
-               "uuid": file_id}
+               "filetype": filetype, "datatype": datatype}
     higlass_keys = tibanna.s3.get_higlass_key()
     if not isinstance(higlass_keys, dict):
         raise Exception("Bad higlass keys found: %s" % higlass_keys)
