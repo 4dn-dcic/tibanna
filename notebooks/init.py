52649ec23816e6c5bf4e8034c16e778e61622278
SooLee <duplexa@gmail.com>
2018-07-11 15:06:57 -0400

    removed notebook and its requirements

--- notebooks/init.py
+++
@@ -1,9 +0,0 @@
-%load_ext autoreload
-%autoreload 2
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from core import utils
-import boto3
-pd.set_option('display.max_columns',100);pd.set_option('display.max_rows',1000)
-%matplotlib inline
