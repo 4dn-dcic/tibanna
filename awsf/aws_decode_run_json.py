18b849744d67e48f364066d9f0ed85260a2f7b31
SooLee <duplexa@gmail.com>
2018-07-13 18:04:59 -0400

    fix newly introduced env problem

--- awsf/aws_decode_run_json.py
+++ awsf/aws_decode_run_json.py
@@ -69,4 +69,4 @@ with open(env_filename, 'w') as f_env:
     f_env.write("MAIN_CWL={}\n".format(Dict["Job"]["App"]["main_cwl"])) ## main cwl to be run (the other cwl files will be called by this one)
     f_env.write("CWL_FILES=\"{}\"\n".format(' '.join(Dict["Job"]["App"]["other_cwl_files"].split(',')))) ## list of cwl files in an array delimited by a space
     f_env.write("OUTBUCKET={}\n".format(Dict["Job"]["Output"]["output_bucket_directory"]))
-    f_env.write("PUBLIC_POSTRUN_JSON={}\n".format('1' if Dict["config"].get('public_postrun_json', False) else '0')
+    f_env.write("PUBLIC_POSTRUN_JSON={}\n".format('1' if Dict["config"].get('public_postrun_json', False) else '0'))
