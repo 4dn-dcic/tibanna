64964b1c2bae4030e96a9695197df34071f6d0b3
SooLee <duplexa@gmail.com>
2018-07-19 16:32:21 -0400

    changed name for utils

---
+++ core/pony_utils.py
@@ -0,0 +1,531 @@
+from __future__ import print_function
+import json
+import boto3
+import os
+import datetime
+from uuid import uuid4
+from dcicutils.ff_utils import (
+    get_metadata,
+    post_metadata,
+    patch_metadata,
+    generate_rand_accession,
+    search_metadata
+)
+from dcicutils.s3_utils import s3Utils
+from core.lambda_utils import run_workflow as _run_workflow
+from core.lambda_utils import _tibanna
+import logging
+
+###########################################
+# These utils exclusively live in Tibanna #
+###########################################
+
+
+###########################
+# Config
+###########################
+
+# logger
+LOG = logging.getLogger(__name__)
+
+
+def create_ffmeta_awsem(workflow, app_name, input_files=None,
+                        parameters=None, title=None, uuid=None,
+                        output_files=None, award='1U01CA200059-01', lab='4dn-dcic-lab',
+                        run_status='started', run_platform='AWSEM', run_url='', tag=None,
+                        aliases=None, awsem_postrun_json=None, submitted_by=None, extra_meta=None,
+                        **kwargs):
+
+    input_files = [] if input_files is None else input_files
+    parameters = [] if parameters is None else parameters
+    if award is None:
+        award = '1U01CA200059-01'
+    if lab is None:
+        lab = '4dn-dcic-lab'
+
+    if title is None:
+        if tag is None:
+            title = app_name + " run " + str(datetime.datetime.now())
+        else:
+            title = app_name + ' ' + tag + " run " + str(datetime.datetime.now())
+
+    return WorkflowRunMetadata(workflow=workflow, app_name=app_name, input_files=input_files,
+                               parameters=parameters, uuid=uuid, award=award,
+                               lab=lab, run_platform=run_platform, run_url=run_url,
+                               title=title, output_files=output_files, run_status=run_status,
+                               aliases=aliases, awsem_postrun_json=awsem_postrun_json,
+                               submitted_by=submitted_by, extra_meta=extra_meta)
+
+
+class WorkflowRunMetadata(object):
+    '''
+    fourfront metadata
+    '''
+
+    def __init__(self, workflow, app_name, input_files=[],
+                 parameters=[], uuid=None,
+                 award='1U01CA200059-01', lab='4dn-dcic-lab',
+                 run_platform='AWSEM', title=None, output_files=None,
+                 run_status='started', awsem_job_id=None,
+                 run_url='', aliases=None, awsem_postrun_json=None,
+                 submitted_by=None, extra_meta=None, **kwargs):
+        """Class for WorkflowRun that matches the 4DN Metadata schema
+        Workflow (uuid of the workflow to run) has to be given.
+        Workflow_run uuid is auto-generated when the object is created.
+        """
+        if run_platform == 'AWSEM':
+            self.awsem_app_name = app_name
+            # self.app_name = app_name
+            if awsem_job_id is None:
+                self.awsem_job_id = ''
+            else:
+                self.awsem_job_id = awsem_job_id
+        else:
+            raise Exception("invalid run_platform {} - it must be AWSEM".format(run_platform))
+
+        self.run_status = run_status
+        self.uuid = uuid if uuid else str(uuid4())
+        self.workflow = workflow
+        self.run_platform = run_platform
+        if run_url:
+            self.run_url = run_url
+
+        self.title = title
+        if aliases:
+            if isinstance(aliases, basestring):  # noqa
+                aliases = [aliases, ]
+            self.aliases = aliases
+        self.input_files = input_files
+        if output_files:
+            self.output_files = output_files
+        self.parameters = parameters
+        self.award = award
+        self.lab = lab
+        if awsem_postrun_json:
+            self.awsem_postrun_json = awsem_postrun_json
+        if submitted_by:
+            self.submitted_by = submitted_by
+
+        if extra_meta:
+            for k, v in extra_meta.iteritems():
+                self.__dict__[k] = v
+
+    def append_outputfile(self, outjson):
+        self.output_files.append(outjson)
+
+    def as_dict(self):
+        return self.__dict__
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def post(self, key, type_name=None):
+        if not type_name:
+            if self.run_platform == 'AWSEM':
+                type_name = 'workflow_run_awsem'
+            else:
+                raise Exception("cannot determine workflow schema type from the run platform: should be AWSEM.")
+        return post_metadata(self.as_dict(), type_name, key=key)
+
+    def patch(self, key, type_name=None):
+        return patch_metadata(self.as_dict(), key=key)
+
+
+class ProcessedFileMetadata(object):
+    def __init__(self, uuid=None, accession=None, file_format='', lab='4dn-dcic-lab',
+                 extra_files=None, source_experiments=None,
+                 award='1U01CA200059-01', status='to be uploaded by workflow',
+                 md5sum=None, file_size=None, other_fields=None, **kwargs):
+        self.uuid = uuid if uuid else str(uuid4())
+        self.accession = accession if accession else generate_rand_accession()
+        self.status = status
+        self.lab = lab
+        self.award = award
+        self.file_format = file_format
+        if extra_files:
+            self.extra_files = extra_files
+        if source_experiments:
+            self.source_experiments = source_experiments
+        if md5sum:
+            self.md5sum = md5sum
+        if file_size:
+            self.file_size = file_size
+        if other_fields:
+            for field in other_fields:
+                setattr(self, field, other_fields[field])
+
+    def as_dict(self):
+        return self.__dict__
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def post(self, key):
+        return post_metadata(self.as_dict(), "file_processed", key=key, add_on='force_md5')
+
+    def patch(self, key):
+        return patch_metadata(self.as_dict(), key=key, add_on='force_md5')
+
+    @classmethod
+    def get(cls, uuid, key, ff_env=None, check_queue=False, return_data=False):
+        data = get_metadata(uuid,
+                            key=key,
+                            ff_env=ff_env,
+                            add_on='frame=object',
+                            check_queue=check_queue)
+        if type(data) is not dict:
+            raise Exception("unable to find object with unique key of %s" % uuid)
+        if 'FileProcessed' not in data.get('@type', {}):
+            raise Exception("you can only load ProcessedFiles into this object")
+
+        pf = ProcessedFileMetadata(**data)
+        if return_data:
+            return pf, data
+        else:
+            return pf
+
+
+def aslist(x):
+    if isinstance(x, list):
+        return x
+    else:
+        return [x]
+
+
+def ensure_list(val):
+    if isinstance(val, (list, tuple)):
+        return val
+    return [val]
+
+
+def get_extra_file_key(infile_format, infile_key, extra_file_format, fe_map):
+    infile_extension = fe_map.get(infile_format)
+    extra_file_extension = fe_map.get(extra_file_format)
+    return infile_key.replace(infile_extension, extra_file_extension)
+
+
+def get_format_extension_map(ff_keys):
+    try:
+        fp_schema = get_metadata("profiles/file_processed.json", key=ff_keys)
+        fe_map = fp_schema.get('file_format_file_extension')
+    except Exception as e:
+        raise Exception("Can't get format-extension map from file_processed schema. %s\n" % e)
+    return fe_map
+
+
+def get_source_experiment(input_file_uuid, ff_keys, ff_env):
+    """
+    Connects to fourfront and get source experiment info as a unique list
+    Takes a single input file uuid.
+    """
+    pf_source_experiments_set = set()
+    inf_uuids = aslist(input_file_uuid)
+    for inf_uuid in inf_uuids:
+        infile_meta = get_metadata(inf_uuid,
+                                   key=ff_keys,
+                                   ff_env=ff_env,
+                                   add_on='frame=object')
+        if infile_meta.get('experiments'):
+            for exp in infile_meta.get('experiments'):
+                exp_obj = get_metadata(exp,
+                                       key=ff_keys,
+                                       ff_env=ff_env,
+                                       add_on='frame=raw')
+                pf_source_experiments_set.add(exp_obj['uuid'])
+        if infile_meta.get('source_experiments'):
+            # this field is an array of strings, not linkTo's
+            pf_source_experiments_set.update(infile_meta.get('source_experiments'))
+    return list(pf_source_experiments_set)
+
+
+def merge_source_experiments(input_file_uuids, ff_keys, ff_env=None):
+    """
+    Connects to fourfront and get source experiment info as a unique list
+    Takes a list of input file uuids.
+    """
+    pf_source_experiments = set()
+    for input_file_uuid in input_file_uuids:
+        pf_source_experiments.update(get_source_experiment(input_file_uuid, ff_keys, ff_env))
+    return list(pf_source_experiments)
+
+
+class Tibanna(object):
+
+    def __init__(self, env, ff_keys=None, sbg_keys=None, settings=None):
+        self.env = env
+        self.s3 = s3Utils(env=env)
+
+        if not ff_keys:
+            ff_keys = self.s3.get_access_keys()
+        self.ff_keys = ff_keys
+
+        if not settings:
+            settings = {}
+        self.settings = settings
+
+    def get_reporter(self):
+        '''
+        a reporter is a generic name for somethign that reports on the results of each step
+        of the workflow.  For our immediate purposes this will return ffmetadata object
+        (which is a connection to our metadata repository, eventually it should, through
+        polymorphism support any type of reporter a user might develop.
+        '''
+        return None
+
+    def get_runner(self):
+        '''
+        a runner is an object that implements a set api to run the workflow one step at a time.
+        Currently this is sbg for us.
+        '''
+        return None
+
+    def as_dict(self):
+        return {'env': self.env,
+                'settings': self.settings}
+
+
+def _tibanna_settings(settings_patch=None, force_inplace=False, env=''):
+    tibanna = {"run_id": str(uuid4()),
+               "env": env,
+               "url": '',
+               'run_type': 'generic',
+               'run_name': '',
+               }
+    in_place = None
+    if force_inplace:
+        if not settings_patch.get(_tibanna):
+            settings_patch[_tibanna] = {}
+    if settings_patch:
+        in_place = settings_patch.get(_tibanna, None)
+        if in_place is not None:
+            tibanna.update(in_place)
+        else:
+            tibanna.update(settings_patch)
+
+    # generate run name
+    if not tibanna.get('run_name'):
+        # aws doesn't like / in names
+        tibanna['run_name'] = "%s_%s" % (tibanna['run_type'].replace('/', '-'), tibanna['run_id'])
+
+    if in_place is not None:
+        settings_patch[_tibanna] = tibanna
+        return settings_patch
+    else:
+        return {_tibanna: tibanna}
+
+
+def current_env():
+    return os.environ.get('ENV_NAME', 'test')
+
+
+def is_prod():
+    return current_env().lower() == 'prod'
+
+
+class WorkflowFile(object):
+
+    def __init__(self, bucket, key, runner, accession=None, output_type=None,
+                 filesize=None, md5=None):
+        self.bucket = bucket
+        self.key = key
+        self.s3 = s3_utils.s3Utils(self.bucket, self.bucket, self.bucket)
+        self.runner = runner
+        self.accession = accession
+        self.output_type = output_type
+        self.filesize = filesize
+        self.md5 = md5
+
+    @property
+    def status(self):
+        exists = self.s3.does_key_exist(self.key, self.bucket)
+        if exists:
+            return "COMPLETED"
+        else:
+            return "FAILED"
+
+    def read(self):
+        return self.s3.read_s3(self.key).strip()
+
+
+# TODO: refactor this to inherit from an abstrat class called Runner
+# then implement for SBG as well
+class Awsem(object):
+
+    def __init__(self, json):
+        self.args = json['args']
+        self.config = json['config']
+        self.output_s3 = self.args['output_S3_bucket']
+        self.app_name = self.args['app_name']
+        self.output_files_meta = json['ff_meta']['output_files']
+        self.output_info = None
+        if isinstance(json.get('postrunjson'), dict):
+            self.output_info = json['postrunjson']['Job']['Output']['Output files']
+
+    def output_files(self):
+        files = dict()
+        output_types = dict()
+        for x in self.output_files_meta:
+            output_types[x['workflow_argument_name']] = x['type']
+        for k, v in self.args.get('output_target').iteritems():
+            if k in output_types:
+                out_type = output_types[k]
+            else:
+                out_type = None
+            if out_type == 'Output processed file':
+                file_name = v.split('/')[-1]
+                accession = file_name.split('.')[0].strip('/')
+            else:
+                accession = None
+            if self.output_info:
+                md5 = self.output_info[k].get('md5sum', '')
+                filesize = self.output_info[k].get('size', 0)
+                wff = {k: WorkflowFile(self.output_s3, v, self, accession,
+                                       output_type=out_type, filesize=filesize, md5=md5)}
+            else:
+                wff = {k: WorkflowFile(self.output_s3, v, self, accession,
+                                       output_type=out_type)}
+            files.update(wff)
+        return files
+
+    def secondary_output_files(self):
+        files = dict()
+        for k, v in self.args.get('secondary_output_target').iteritems():
+            wff = {k: WorkflowFile(self.output_s3, v, self)}
+            files.update(wff)
+        return files
+
+    def input_files(self):
+        files = dict()
+        for arg_name, item in self.args.get('input_files').iteritems():
+            file_name = item.get('object_key').split('/')[-1]
+            accession = file_name.split('.')[0].strip('/')
+            wff = {arg_name: WorkflowFile(item.get('bucket_name'),
+                                          item.get('object_key'),
+                                          self,
+                                          accession)}
+            files.update(wff)
+        return files
+
+    def all_files(self):
+        files = dict()
+        files.update(self.input_files())
+        files.update(self.output_files())
+        return files
+
+    @property
+    def inputfile_accessions(self):
+        return {k: v.accession for k, v in self.input_files().iteritems()}
+
+    @property
+    def all_file_accessions(self):
+        return {k: v.accession for k, v in self.all_files().iteritems()}
+
+
+def run_md5(env, accession, uuid):
+    tibanna = Tibanna(env=env)
+    meta_data = get_metadata(accession, key=tibanna.ff_keys)
+    file_name = meta_data['upload_key'].split('/')[-1]
+
+    input_json = make_input(env=env, workflow='md5', object_key=file_name, uuid=uuid)
+    return _run_workflow(input_json, accession)
+
+
+def batch_fastqc(env, batch_size=20):
+    '''
+    try to run fastqc on everythign that needs it ran
+    '''
+    files_processed = 0
+    files_skipped = 0
+
+    # handle ctrl-c
+    import signal
+
+    def report(signum, frame):
+        print("Processed %s files, skipped %s files" % (files_processed, files_skipped))
+        sys.exit(-1)
+
+    signal.signal(signal.SIGINT, report)
+
+    tibanna = Tibanna(env=env)
+    uploaded_files = search_metadata("search/?type=File&status=uploaded&limit=%s" % batch_size,
+                                     key=tibanna.ff_key, ff_env=tibanna.env)
+
+    # TODO: need to change submit 4dn to not overwrite my limit
+    if len(uploaded_files['@graph']) > batch_size:
+        limited_files = uploaded_files['@graph'][:batch_size]
+    else:
+        limited_files = uploaded_files['@graph']
+
+    for ufile in limited_files:
+        fastqc_run = False
+        for wfrun in ufile.get('workflow_run_inputs', []):
+            if 'fastqc' in wfrun:
+                fastqc_run = True
+        if not fastqc_run:
+            print("running fastqc for %s" % ufile.get('accession'))
+            run_fastqc(env, ufile.get('accession'), ufile.get('uuid'))
+            files_processed += 1
+        else:
+            print("******** fastqc already run for %s skipping" % ufile.get('accession'))
+            files_skipped += 1
+        sleep(5)
+        if files_processed % 10 == 0:
+            sleep(60)
+
+    print("Processed %s files, skipped %s files" % (files_processed, files_skipped))
+
+
+def run_fastqc(env, accession, uuid):
+    if not accession.endswith(".fastq.gz"):
+        accession += ".fastq.gz"
+    input_json = make_input(env=env, workflow='fastqc-0-11-4-1', object_key=accession, uuid=uuid)
+    return _run_workflow(input_json, accession)
+
+
+_workflows = {'md5':
+              {'uuid': 'd3f25cd3-e726-4b3c-a022-48f844474b41',
+               'arg_name': 'input_file'
+               },
+              'fastqc-0-11-4-1':
+              {'uuid': '2324ad76-ff37-4157-8bcc-3ce72b7dace9',
+               'arg_name': 'input_fastq'
+               },
+              }
+
+
+def make_input(env, workflow, object_key, uuid):
+    bucket = "elasticbeanstalk-%s-files" % env
+    output_bucket = "elasticbeanstalk-%s-wfoutput" % env
+    workflow_uuid = _workflows[workflow]['uuid']
+    workflow_arg_name = _workflows[workflow]['arg_name']
+
+    data = {"parameters": {},
+            "app_name": workflow,
+            "workflow_uuid": workflow_uuid,
+            "input_files": [
+                {"workflow_argument_name": workflow_arg_name,
+                 "bucket_name": bucket,
+                 "uuid": uuid,
+                 "object_key": object_key,
+                 }
+             ],
+            "output_bucket": output_bucket,
+            "config": {
+                "ebs_type": "io1",
+                "json_bucket": "4dn-aws-pipeline-run-json",
+                "ebs_iops": 500,
+                "shutdown_min": 30,
+                "ami_id": "ami-cfb14bb5",
+                "copy_to_s3": True,
+                "script_url": "https://raw.githubusercontent.com/4dn-dcic/tibanna/master/awsf/",
+                "launch_instance": True,
+                "password": "thisisnotmypassword",
+                "log_bucket": "tibanna-output",
+                "key_name": ""
+              },
+            }
+    data.update(_tibanna_settings({'run_id': str(object_key),
+                                   'run_type': workflow,
+                                   'env': env,
+                                   }))
+    return data
