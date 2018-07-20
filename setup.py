872ea1a197ecaf7dc8576306932e16fd521d9dd2
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-07-20 18:47:41 -0400

    update core version; use empty setup_requires and install_requires

--- setup.py
+++ setup.py
@@ -8,20 +8,21 @@ try:
 except:
     pass  # don't know why this fails with tox
 
-# minimum requirements to create the core package. Covers pony and unicorn
-with open('requirements-lambda-pony.txt') as f:
+# for the time being, we are not pip packaging, so do not use
+# setup_requires or install_requires
+with open('requirements-lambda-unicorn.txt') as f:
     inst_parsed = f.read().splitlines()
-install_requires = [req.strip() for req in inst_parsed if '-e git' not in req]
+install_requires = [req.strip() for req in inst_parsed if 'git+' not in req]
 
 # full requirements for unicorn (does not require dcicutils)
 with open('requirements.txt') as f:
     set_parsed = f.read().splitlines()
-setup_requires = [req.strip() for req in set_parsed if '-e git' not in req]
+setup_requires = [req.strip() for req in set_parsed if 'git+' not in req]
 
 # full requirements for pony and running tests (includes dcicutils)
 with open('requirements-4dn.txt') as f:
     tests_parsed = f.read().splitlines()
-tests_require = [req.strip() for req in tests_parsed if '-e git' not in req]
+tests_require = [req.strip() for req in tests_parsed if 'git+' not in req]
 
 
 setup(
@@ -40,13 +41,13 @@ setup(
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
             ],
-    install_requires=install_requires,
+    install_requires=[],
     include_package_data=True,
     tests_require=tests_require,
     extras_require={
         'test': tests_require,
     },
-    setup_requires=setup_requires,
+    setup_requires=[],
     dependency_links=[
         'git+https://github.com/4dn-dcic/python-lambda.git#egg=python_lambda',
         'git+https://github.com/SooLee/Benchmark.git#egg=Benchmark'
