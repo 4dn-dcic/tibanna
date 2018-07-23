f3d583a1d17f65d0222231f0b0992368cbc2dfb7
SooLee <duplexa@gmail.com>
2018-07-23 22:36:21 +0000

    temporary awsf logging

--- awsf_cwl_draft3/aws_run_workflow.sh
+++ awsf_cwl_draft3/aws_run_workflow.sh
@@ -89,8 +89,8 @@ exl date  ## start logging
 ### sshd configure for password recognition
 if [ ! -z $PASSWORD ]; then
   echo -ne "$PASSWORD\n$PASSWORD\n" | sudo passwd ec2-user
-  send_log
   sed 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config | sed 's/#PasswordAuthentication no/PasswordAuthentication yes/g' > tmpp
+  send_log
   mv tmpp /etc/ssh/sshd_config
   exl service sshd restart
 fi
