78fe442ccfbae25359e3f2dfd8a3f6a36208f9d9
SooLee <duplexa@gmail.com>
2018-07-30 21:02:49 +0000

    validate_md5_s3_trigger now works for extra files too

--- core/validate_md5_s3_trigger/service.py
+++ core/validate_md5_s3_trigger/service.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import boto3
 from core.utils import _tibanna_settings, STEP_FUNCTION_ARN
-from core.pony_utils import Tibanna
+from core.pony_utils import Tibanna, get_format_extension_map
 from dcicutils.ff_utils import get_metadata
 import json
 
@@ -22,23 +22,70 @@ def handler(event, context):
     if event.get('run_name'):
         run_name = event.get('run_name')  # used for testing
 
-    # only run if status is uploading...
-    is_uploading = is_status_uploading(event)
-    if event.get('force_run'):
-        is_uploading = True
-    if is_uploading:
-        # trigger the step function to run
+    extra_file_format = get_extra_file_format(event)
+    if extra_file_format:
+        # for extra file-triggered md5 run, status check is skipped.
+        input_json = json.dumps(make_input(event)
+        input_json['input_files'][0]['format_if_extra'] = extra_file_format
         response = client.start_execution(
             stateMachineArn=STEP_FUNCTION_ARN,
             name=run_name,
-            input=json.dumps(make_input(event)),
+            input=json.dumps(input_json),
         )
-
-        # pop no json serializable stuff...
-        response.pop('startDate')
-        return response
     else:
-        return {'info': 'status is not uploading'}
+        # only run if status is uploading...
+        is_uploading = is_status_uploading(event)
+        if event.get('force_run'):
+            is_uploading = True
+        if is_uploading:
+            # trigger the step function to run
+            response = client.start_execution(
+                stateMachineArn=STEP_FUNCTION_ARN,
+                name=run_name,
+                input=json.dumps(make_input(event)),
+            )
+        else:
+            return {'info': 'status is not uploading'}
+
+    # pop no json serializable stuff...
+    response.pop('startDate')
+    return response
+
+
+def get_extra_file_format(event):
+    '''if the file extension matches the regular file format,
+    returns None
+    if it matches one of the format of an extra file,
+    returns that format (e.g. 'pairs_px2'
+    '''
+    # guess env from bucket name
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    env = '-'.join(bucket.split('-')[1:3])
+    upload_key = event['Records'][0]['s3']['object']['key']
+    uuid, object_key = upload_key.split('/')
+    accession = object_key.split('.')[0]
+    extension = object_key.replace(accession, '')
+
+    tibanna = Tibanna(env=env)
+    meta = get_metadata(accession,
+                        key=tibanna.ff_keys,
+                        ff_env=env,
+                        add_on='frame=object',
+                        check_queue=True)
+    if meta:
+        file_format = meta.get('file_format')
+        fe_map = get_format_extension_map()
+        file_extension = fe_map.get(file_format)
+        if extension == file_extension:
+            return None
+        else:
+            for extra in meta.get('extra_files', []):
+                extra_format = extra.get('file_format')
+                extra_extension = fe_map.get(extra_format)
+                if extension == extra_extension:
+                    return extra_format
+        else:
+            raise Exception("file extension not matching")
 
 
 def is_status_uploading(event):
