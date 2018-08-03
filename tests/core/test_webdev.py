912e5d693ee2eac0f05e015e44b783d7bd103f52
SooLee <duplexa@gmail.com>
2018-08-03 15:48:44 +0000

    test extra first

--- tests/core/test_webdev.py
+++ tests/core/test_webdev.py
@@ -52,65 +52,6 @@ def post_random_file(bucket, ff_key):
     return newfile
 
 
-def testrun_md5(workflow_name='tibanna_pony', env='webdev'):
-    """Creates a random file object with no md5sum/content_md5sum and run md5 workflow.
-    It waits for 6 mintues till the workflow run finishes and checks the input file object
-    has been updated.
-    """
-    bucket = "elasticbeanstalk-fourfront-" + env + "-wfoutput"
-    ff_key = get_authentication_with_server(ff_env='fourfront-' + env)
-    newfile = post_random_file(bucket, ff_key)
-    uuid = newfile['uuid']
-    accession = newfile['accession']
-    input_json = {
-        "config": {
-          "ebs_type": "io1",
-          "ebs_iops": 500,
-          "s3_access_arn": "arn:aws:iam::643366669028:instance-profile/S3_access",
-          "ami_id": "ami-cfb14bb5",
-          "json_bucket": "4dn-aws-pipeline-run-json",
-          "shutdown_min": 30,
-          "copy_to_s3": True,
-          "launch_instance": True,
-          "log_bucket": "tibanna-output",
-          "script_url": "https://raw.githubusercontent.com/4dn-dcic/tibanna/master/awsf/",
-          "key_name": "4dn-encode",
-          "password": ""
-        },
-        "_tibanna": {
-          "env": "fourfront-webdev",
-          "run_type": "md5"
-        },
-        "parameters": {},
-        "app_name": "md5",
-        "workflow_uuid": "c77a117b-9a58-477e-aaa5-291a109a99f6",
-        "input_files": [
-                        {"workflow_argument_name": "input_file",
-                         "bucket_name": bucket,
-                         "uuid": uuid,
-                         "object_key": accession + '.pairs.gz'
-                         }
-        ],
-        "output_bucket": bucket,
-        "wfr_meta": {"notes": "processed file md5 trigger test from test_webdev.py"}
-    }
-    resp = run_workflow(input_json, workflow=workflow_name)
-    print(resp)
-
-    # check result
-    time.sleep(6*60)  # wait for 6 minutes
-    filemeta = get_metadata(uuid, key=ff_key, add_on='?datastore=database')
-    assert 'md5sum' in filemeta
-    assert 'content_md5sum' in filemeta
-    content_md5sum = filemeta.get('content_md5sum')
-    md5sum = filemeta.get('md5sum')
-    assert md5sum
-    assert content_md5sum
-    print(content_md5sum)
-    print(md5sum)
-    patch_metadata({'status': 'deleted'}, uuid, key=ff_key)
-
-
 def testrun_md5_input_json_w_extra_file_object_name(workflow_name='tibanna_pony', env='webdev'):
     """Creates a random file object with no md5sum/content_md5sum and run md5 workflow.
     It waits for 6 mintues till the workflow run finishes and checks the input file object
@@ -168,6 +109,65 @@ def testrun_md5_input_json_w_extra_file_object_name(workflow_name='tibanna_pony'
     assert 'input_files' in wfr_meta
     assert 'input_file' in wfr_meta['input_files']
     assert 'format_if_extra' in wfr_meta['input_files']['input_file']
+    assert md5sum
+    assert content_md5sum
+    print(content_md5sum)
+    print(md5sum)
+    patch_metadata({'status': 'deleted'}, uuid, key=ff_key)
+
+
+def testrun_md5(workflow_name='tibanna_pony', env='webdev'):
+    """Creates a random file object with no md5sum/content_md5sum and run md5 workflow.
+    It waits for 6 mintues till the workflow run finishes and checks the input file object
+    has been updated.
+    """
+    bucket = "elasticbeanstalk-fourfront-" + env + "-wfoutput"
+    ff_key = get_authentication_with_server(ff_env='fourfront-' + env)
+    newfile = post_random_file(bucket, ff_key)
+    uuid = newfile['uuid']
+    accession = newfile['accession']
+    input_json = {
+        "config": {
+          "ebs_type": "io1",
+          "ebs_iops": 500,
+          "s3_access_arn": "arn:aws:iam::643366669028:instance-profile/S3_access",
+          "ami_id": "ami-cfb14bb5",
+          "json_bucket": "4dn-aws-pipeline-run-json",
+          "shutdown_min": 30,
+          "copy_to_s3": True,
+          "launch_instance": True,
+          "log_bucket": "tibanna-output",
+          "script_url": "https://raw.githubusercontent.com/4dn-dcic/tibanna/master/awsf/",
+          "key_name": "4dn-encode",
+          "password": ""
+        },
+        "_tibanna": {
+          "env": "fourfront-webdev",
+          "run_type": "md5"
+        },
+        "parameters": {},
+        "app_name": "md5",
+        "workflow_uuid": "c77a117b-9a58-477e-aaa5-291a109a99f6",
+        "input_files": [
+                        {"workflow_argument_name": "input_file",
+                         "bucket_name": bucket,
+                         "uuid": uuid,
+                         "object_key": accession + '.pairs.gz'
+                         }
+        ],
+        "output_bucket": bucket,
+        "wfr_meta": {"notes": "processed file md5 trigger test from test_webdev.py"}
+    }
+    resp = run_workflow(input_json, workflow=workflow_name)
+    print(resp)
+
+    # check result
+    time.sleep(6*60)  # wait for 6 minutes
+    filemeta = get_metadata(uuid, key=ff_key, add_on='?datastore=database')
+    assert 'md5sum' in filemeta
+    assert 'content_md5sum' in filemeta
+    content_md5sum = filemeta.get('content_md5sum')
+    md5sum = filemeta.get('md5sum')
     assert md5sum
     assert content_md5sum
     print(content_md5sum)
