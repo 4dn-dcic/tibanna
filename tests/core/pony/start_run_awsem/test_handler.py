e2219ab56d3e0fcb6c3a18e7b676f0b78e62772c
SooLee <duplexa@gmail.com>
2018-08-27 14:48:45 -0400

    codacy

--- tests/core/pony/start_run_awsem/test_handler.py
+++ tests/core/pony/start_run_awsem/test_handler.py
@@ -68,7 +68,7 @@ def test_user_supplied_proc_file(run_awsem_event_data_processed_files, proc_file
     file_with_type = proc_file_in_webdev.copy()
     file_with_type['@type'] = ['FileProcessed', 'Item', 'whatever']
     with mock.patch('core.pony_utils.get_metadata', return_value=file_with_type):
-        pf, resp = user_supplied_proc_file(of, 'output_file1', tibanna)
+        pf, _ = user_supplied_proc_file(of, 'output_file1', tibanna)
         assert type(pf) == ProcessedFileMetadata
         assert pf.__dict__ == proc_file_in_webdev
 
