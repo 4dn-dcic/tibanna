97a02ded1f7c340dddb1777ca823ad2fc7199512
SooLee <duplexa@gmail.com>
2018-11-19 22:27:02 +0000

    .

--- tests/awsf/test.sh
+++ tests/awsf/test.sh
@@ -16,6 +16,7 @@ python awsf/aws_decode_run_json.py tests/awsf/wdl/uGeIte1giKxt.run.json  # doubl
 python awsf/aws_upload_output_update_json.py tests/awsf/bqLd8oa7Tdzq.run.json tests/awsf/bqLd8oa7Tdzq.log.json tests/awsf/bqLd8oa7Tdzq.LOG tests/awsf/bqLd8oa7Tdzq.md5sum.txt tests/awsf/bqLd8oa7Tdzq.postrun.json.out
 
 # test for wdl
+# commented out since they will produce 'file not found error'. They work only if the files to upload exist.
 #python awsf/aws_upload_output_update_json.py tests/awsf/wdl/a3T0RlZ09WuR.run.json tests/awsf/wdl/a3T0RlZ09WuR.log.json tests/awsf/wdl/a3T0RlZ09WuR.log tests/awsf/wdl/a3T0RlZ09WuR.md5sum.txt tests/awsf/wdl/a3T0RlZ09WuR.postrun.json.out wdl
-python awsf/aws_upload_output_update_json.py tests/awsf/wdl/a3T0RlZ09WuS.run.json tests/awsf/wdl/a3T0RlZ09WuS.log.json tests/awsf/wdl/a3T0RlZ09WuS.log tests/awsf/wdl/a3T0RlZ09WuS.md5sum.txt tests/awsf/wdl/a3T0RlZ09WuS.postrun.json.out wdl  # test for alt_cond_output_argnames
+#python awsf/aws_upload_output_update_json.py tests/awsf/wdl/a3T0RlZ09WuS.run.json tests/awsf/wdl/a3T0RlZ09WuS.log.json tests/awsf/wdl/a3T0RlZ09WuS.log tests/awsf/wdl/a3T0RlZ09WuS.md5sum.txt tests/awsf/wdl/a3T0RlZ09WuS.postrun.json.out wdl  # test for alt_cond_output_argnames
 
