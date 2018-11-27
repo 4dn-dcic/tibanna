11df183ec32554b9f09a31096c340e75878a029b
SooLee <duplexa@gmail.com>
2018-11-27 23:32:43 +0000

    skip trigger for non4DN accession

--- core/validate_md5_s3_trigger/service.py
+++ core/validate_md5_s3_trigger/service.py
@@ -14,12 +14,14 @@ def handler(event, context):
         event["Records"]["eventTime"] = str(event["Records"]["eventTime"])
 
     upload_key = event['Records'][0]['s3']['object']['key']
-    run_name = upload_key.split('/')[1].split('.')[0]
-
+    accession = upload_key.split('/')[1].split('.')[0]
+    if not accession.startswith('4DN'):
+        printlog("Skipping trigger: not 4DN accession %s" % accession)
+        return event
     client = boto3.client('stepfunctions', region_name=AWS_REGION)
     response = client.start_execution(
         stateMachineArn=STEP_FUNCTION_ARN(INITIATOR_STEP_FUNCTION_NAME),
-        name=run_name + '_' + str(uuid.uuid4()),
+        name=accession + '_' + str(uuid.uuid4()),
         input=json.dumps(event),
     )
     printlog(str(response))
