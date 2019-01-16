3b1547f04b265b990648673f7a98975bfef7018f
SooLee <duplexa@gmail.com>
2019-01-16 17:30:21 +0000

    minor modification for benchmark test

--- tests/core/unicorn/test_ec2_utils.py
+++ tests/core/unicorn/test_ec2_utils.py
@@ -13,7 +13,7 @@ def test_update_config(run_task_awsem_event_data):
     config = data['config']
     update_config(config, data['args']['app_name'], data['args']['input_files'], data['args']['input_parameters'])
     assert config['instance_type'] == 't3.micro'
-    assert config['EBS_optimized'] is False
+    assert config['EBS_optimized'] is True
     assert config['ebs_size'] >= 10
     assert config['shutdown_min'] == 30  # check the other fields are preserved in the returned config
 
@@ -23,7 +23,7 @@ def test_update_config2(run_task_awsem_event_data2):
     config = data['config']
     update_config(config, data['args']['app_name'], data['args']['input_files'], data['args']['input_parameters'])
     assert config['instance_type'] == 't3.xlarge'
-    assert config['EBS_optimized'] is False
+    assert config['EBS_optimized'] is True
     assert config['ebs_size'] >= 10
     assert config['shutdown_min'] == 30  # check the other fields are preserved in the returned config
 
