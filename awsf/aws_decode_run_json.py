fc9c0d11b59560c1a646d1c4a91b4cc74850d38a
SooLee <duplexa@gmail.com>
2019-06-01 01:58:02 +0000

    fixed error passing command and container image

--- awsf/aws_decode_run_json.py
+++ awsf/aws_decode_run_json.py
@@ -156,23 +156,20 @@ def create_env_def_file(env_filename, Dict, language):
     with open(env_filename, 'w') as f_env:
         if language == 'wdl':
             f_env.write("export WDL_URL={}\n".format(Dict["Job"]["App"]["wdl_url"]))
-            # main cwl to be run (the other cwl files will be called by this one)
             f_env.write("export MAIN_WDL={}\n".format(Dict["Job"]["App"]["main_wdl"]))
-            # list of cwl files in an array delimited by a space
             f_env.write("export WDL_FILES=\"{}\"\n".format(' '.join(Dict["Job"]["App"]["other_wdl_files"].split(','))))
         elif language == 'snakemake':
             f_env.write("export SNAKEMAKE_URL={}\n".format(Dict["Job"]["App"]["snakemake_url"]))
-            # main cwl to be run (the other cwl files will be called by this one)
             f_env.write("export MAIN_SNAKEMAKE={}\n".format(Dict["Job"]["App"]["main_snakemake"]))
-            # list of cwl files in an array delimited by a space
             f_env.write("export SNAKEMAKE_FILES=\"{}\"\n".format(' '.join(Dict["Job"]["App"]["other_snakemake_files"].split(','))))
+            f_env.write("export COMMAND=\"{}\"\n".format(Dict["Job"]["App"]["command"]))
+            f_env.write("export CONTAINER_IMAGE=\"{}\"\n".format(Dict["Job"]["App"]["container_image"]))
         elif language == 'shell':
             f_env.write("export COMMAND=\"{}\"\n".format(Dict["Job"]["App"]["command"]))
+            f_env.write("export CONTAINER_IMAGE=\"{}\"\n".format(Dict["Job"]["App"]["container_image"]))
         else:  # cwl
             f_env.write("export CWL_URL={}\n".format(Dict["Job"]["App"]["cwl_url"]))
-            # main cwl to be run (the other cwl files will be called by this one)
             f_env.write("export MAIN_CWL={}\n".format(Dict["Job"]["App"]["main_cwl"]))
-            # list of cwl files in an array delimited by a space
             f_env.write("export CWL_FILES=\"{}\"\n".format(' '.join(Dict["Job"]["App"]["other_cwl_files"].split(','))))
         # other env variables
         f_env.write("export OUTBUCKET={}\n".format(Dict["Job"]["Output"]["output_bucket_directory"]))
