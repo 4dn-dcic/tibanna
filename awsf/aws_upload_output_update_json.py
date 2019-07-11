0369fb708e734f98143fd250fea74b7703a296d0
SooLee <duplexa@gmail.com>
2019-07-11 16:58:21 +0000

    logging the output target dir not working

--- awsf/aws_upload_output_update_json.py
+++ awsf/aws_upload_output_update_json.py
@@ -48,6 +48,9 @@ def upload_to_s3(s3, source, bucket, target):
                 source_f = os.path.join(root, f)
                 target_subdir = re.sub(source + '/', '', root)
                 target_f = os.path.join(target_subdir, f)
+                print("source_f=" + source_f)
+                print("target_subdir=" + target_subdir)
+                print("target_f" + target_f)
                 s3.upload_file(source_f, output_bucket, target_f)
             # for d in dirs:
             #     source_d = os.path.join(root, d)
