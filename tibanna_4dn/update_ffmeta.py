76e326e7589c0d42a86a844c20f430a9e03ccfa2
SooLee <duplexa@gmail.com>
2019-08-06 15:36:44 +0000

    flake8

--- tibanna_4dn/update_ffmeta.py
+++ tibanna_4dn/update_ffmeta.py
@@ -2,16 +2,17 @@
 import copy
 from .pony_utils import FourfrontUpdater
 
+
 def update_ffmeta(input_json):
     """Check output and update fourfront metadata"""
     input_json_copy = copy.deepcopy(input_json)
-    
+
     # metadata-only info may be in 'metadat_only' or in 'config'->'runmode'->'metadata_only'
     # if metadata_only is True, that means the job did not actually run - we're creating/updating metadata
     # as if the job has run.
     if not input_json_copy.get('metadata_only', False):
         input_json_copy['metadata_only'] = input_json_copy['config'].get('runmode', {}).get('metadata_only', False)
-   
+
     # actual metadata update
     updater = FourfrontUpdater(**input_json_copy)
     if input_json_copy.get('error', False):
@@ -20,7 +21,7 @@ def update_ffmeta(input_json):
         updater.update_metadata()
     except Exception as e:
         updater.handle_error(str(e))
-    
+
     # lambda output
     input_json_copy['ff_meta'] = updater.ff_meta.as_dict()
     input_json_copy['pf_meta'] = [v.as_dict() for _, v in updater.pf_output_files.items()]
