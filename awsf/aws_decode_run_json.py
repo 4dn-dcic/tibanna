97b69e79b88fffbb2bc203424c1381825eb36edf
Soo Lee <duplexa@gmail.com>
2019-11-22 11:13:16 -0500

    Update aws_decode_run_json.py

--- awsf/aws_decode_run_json.py
+++ awsf/aws_decode_run_json.py
@@ -10,42 +10,37 @@ INPUT_DIR = "/data1/input"
 
 
 def main():
+    """reads a run json file and creates three text files:
+    download command list file (commands to download input files from s3)
+    input yml file (for cwl/wdl/snakemake run)
+    env list file (environment variables to be sourced)
+    """
     # read json file
     with open(sys.argv[1], 'r') as json_file:
-        Dict = json.load(json_file)
-        Dict_input = Dict["Job"]["Input"]
-        language = Dict["Job"]["App"]["language"]
+        d = json.load(json_file)
+    d_input = d["Job"]["Input"]
+    language = d["Job"]["App"]["language"]
+
     # create a download command list file from the information in json
-    create_download_command_list(downloadlist_filename, Dict_input, language)
+    create_download_command_list(downloadlist_filename, d_input, language)
+
     # create an input yml file to be used on awsem
     if language == 'wdl':  # wdl
-        create_input_for_wdl(input_yml_filename, Dict_input)
-    elif language == 'snakemake':  # wdl
-        create_input_for_snakemake(input_yml_filename, Dict_input)
+        create_input_for_wdl(input_yml_filename, d_input)
+    elif language == 'snakemake':  # snakemake
+        create_input_for_snakemake(input_yml_filename, d_input)
     else:  # cwl
-        create_input_for_cwl(input_yml_filename, Dict_input)
+        create_input_for_cwl(input_yml_filename, d_input)
+
     # create a file that defines environmental variables
-    create_env_def_file(env_filename, Dict, language)
+    create_env_def_file(env_filename, d, language)
 
 
-def add_download_cmd(data_bucket, data_file, target, profile_flag, f, unzip):
-    if data_file:
-        if data_file.endswith('/'):
-            data_file = data_file.rstrip('/')
-        if not unzip:
-            cmd = "if [[ -z $(aws s3 ls s3://{0}/{1}/) ]]; then aws s3 cp s3://{0}/{1} {2} {3}; else aws s3 cp --recursive s3://{0}/{1} {2} {3}; fi\n"
-        elif unzip == 'gz':
-            cmd = "if [[ -z $(aws s3 ls s3://{0}/{1}/) ]]; then aws s3 cp s3://{0}/{1} {2} {3}; gunzip {2}; else aws s3 cp --recursive s3://{0}/{1} {2} {3}; for f in `find {2} -type f`; do if [[ $f =~ \.gz$ ]]; then gunzip $f; fi; done; fi\n"
-	elif unzip == 'bz2':
-            cmd = "if [[ -z $(aws s3 ls s3://{0}/{1}/) ]]; then aws s3 cp s3://{0}/{1} {2} {3}; bzip2 -d {2}; else aws s3 cp --recursive s3://{0}/{1} {2} {3}; for f in `find {2} -type f`; do if [[ $f =~ \.bz2$ ]]; then bzip2 -d $f; fi; done; fi\n"
-	f.write(cmd.format(data_bucket, data_file, target, profile_flag))
-
-
-# create a download command list file from the information in json
-def create_download_command_list(downloadlist_filename, Dict_input, language):
+def create_download_command_list(downloadlist_filename, d_input, language):
+    """create a download command list file from the information in json"""
     with open(downloadlist_filename, 'w') as f:
         for category in ["Input_files_data", "Secondary_files_data"]:
-            for inkey, v in Dict_input[category].iteritems():
+            for inkey, v in d_input[category].iteritems():
                 if inkey.startswith('file://'):
                     if language not in ['shell', 'snakemake']:
                         raise Exception('input file has to be defined with argument name for CWL and WDL')
@@ -90,6 +85,19 @@ def create_download_command_list(downloadlist_filename, Dict_input, language):
                     add_download_cmd(data_bucket, data_file, target, profile_flag, f, unzip)
 
 
+def add_download_cmd(data_bucket, data_file, target, profile_flag, f, unzip):
+    if data_file:
+        if data_file.endswith('/'):
+            data_file = data_file.rstrip('/')
+        if not unzip:
+            cmd = "if [[ -z $(aws s3 ls s3://{0}/{1}/) ]]; then aws s3 cp s3://{0}/{1} {2} {3}; else aws s3 cp --recursive s3://{0}/{1} {2} {3}; fi\n"
+        elif unzip == 'gz':
+            cmd = "if [[ -z $(aws s3 ls s3://{0}/{1}/) ]]; then aws s3 cp s3://{0}/{1} {2} {3}; gunzip {2}; else aws s3 cp --recursive s3://{0}/{1} {2} {3}; for f in `find {2} -type f`; do if [[ $f =~ \.gz$ ]]; then gunzip $f; fi; done; fi\n"
+	elif unzip == 'bz2':
+            cmd = "if [[ -z $(aws s3 ls s3://{0}/{1}/) ]]; then aws s3 cp s3://{0}/{1} {2} {3}; bzip2 -d {2}; else aws s3 cp --recursive s3://{0}/{1} {2} {3}; for f in `find {2} -type f`; do if [[ $f =~ \.bz2$ ]]; then bzip2 -d $f; fi; done; fi\n"
+	f.write(cmd.format(data_bucket, data_file, target, profile_flag))
+
+
 def file2cwlfile(filename, dir, unzip):
     if unzip:
         filename = re.match('(.+)\.{0}$'.format(unzip), filename).group(1)
@@ -103,9 +111,9 @@ def file2wdlfile(filename, dir, unzip):
 
 
 # create an input yml file for cwl-runner
-def create_input_for_cwl(input_yml_filename, Dict_input):
+def create_input_for_cwl(input_yml_filename, d_input):
     with open(input_yml_filename, 'w') as f_yml:
-        inputs = Dict_input.copy()
+        inputs = d_input.copy()
         yml = {}
         for category in ["Input_parameters"]:
             for item, value in inputs[category].iteritems():
@@ -151,9 +159,9 @@ def create_input_for_cwl(input_yml_filename, Dict_input):
         json.dump(yml, f_yml, indent=4, sort_keys=True)
 
 
-def create_input_for_wdl(input_yml_filename, Dict_input):
+def create_input_for_wdl(input_yml_filename, d_input):
     with open(input_yml_filename, 'w') as f_yml:
-        inputs = Dict_input.copy()
+        inputs = d_input.copy()
         yml = {}
         for category in ["Input_parameters"]:
             for item, value in inputs[category].iteritems():
@@ -192,39 +200,39 @@ def create_input_for_wdl(input_yml_filename, Dict_input):
         json.dump(yml, f_yml, indent=4, sort_keys=True)
 
 
-def create_input_for_snakemake(input_yml_filename, Dict_input):
+def create_input_for_snakemake(input_yml_filename, d_input):
     pass  # for now assume no input yml
 
 
 # create a file that defines environmental variables
-def create_env_def_file(env_filename, Dict, language):
+def create_env_def_file(env_filename, d, language):
     # I have to use these variables after this script finishes running.
     # I didn't use os.environ + os.system('bash') because that would remove the other
     # env variables set before this script started running.
     with open(env_filename, 'w') as f_env:
         if language == 'wdl':
-            f_env.write("export WDL_URL={}\n".format(Dict["Job"]["App"]["wdl_url"]))
-            f_env.write("export MAIN_WDL={}\n".format(Dict["Job"]["App"]["main_wdl"]))
-            f_env.write("export WDL_FILES=\"{}\"\n".format(' '.join(Dict["Job"]["App"]["other_wdl_files"].split(','))))
+            f_env.write("export WDL_URL={}\n".format(d["Job"]["App"]["wdl_url"]))
+            f_env.write("export MAIN_WDL={}\n".format(d["Job"]["App"]["main_wdl"]))
+            f_env.write("export WDL_FILES=\"{}\"\n".format(' '.join(d["Job"]["App"]["other_wdl_files"].split(','))))
         elif language == 'snakemake':
-            f_env.write("export SNAKEMAKE_URL={}\n".format(Dict["Job"]["App"]["snakemake_url"]))
-            f_env.write("export MAIN_SNAKEMAKE={}\n".format(Dict["Job"]["App"]["main_snakemake"]))
-            f_env.write("export SNAKEMAKE_FILES=\"{}\"\n".format(' '.join(Dict["Job"]["App"]["other_snakemake_files"].split(','))))
-            f_env.write("export COMMAND=\"{}\"\n".format(Dict["Job"]["App"]["command"].replace("\"", "\\\"")))
-            f_env.write("export CONTAINER_IMAGE={}\n".format(Dict["Job"]["App"]["container_image"]))
+            f_env.write("export SNAKEMAKE_URL={}\n".format(d["Job"]["App"]["snakemake_url"]))
+            f_env.write("export MAIN_SNAKEMAKE={}\n".format(d["Job"]["App"]["main_snakemake"]))
+            f_env.write("export SNAKEMAKE_FILES=\"{}\"\n".format(' '.join(d["Job"]["App"]["other_snakemake_files"].split(','))))
+            f_env.write("export COMMAND=\"{}\"\n".format(d["Job"]["App"]["command"].replace("\"", "\\\"")))
+            f_env.write("export CONTAINER_IMAGE={}\n".format(d["Job"]["App"]["container_image"]))
         elif language == 'shell':
-            f_env.write("export COMMAND=\"{}\"\n".format(Dict["Job"]["App"]["command"].replace("\"", "\\\"")))
-            f_env.write("export CONTAINER_IMAGE={}\n".format(Dict["Job"]["App"]["container_image"]))
+            f_env.write("export COMMAND=\"{}\"\n".format(d["Job"]["App"]["command"].replace("\"", "\\\"")))
+            f_env.write("export CONTAINER_IMAGE={}\n".format(d["Job"]["App"]["container_image"]))
         else:  # cwl
-            f_env.write("export CWL_URL={}\n".format(Dict["Job"]["App"]["cwl_url"]))
-            f_env.write("export MAIN_CWL={}\n".format(Dict["Job"]["App"]["main_cwl"]))
-            f_env.write("export CWL_FILES=\"{}\"\n".format(' '.join(Dict["Job"]["App"]["other_cwl_files"].split(','))))
+            f_env.write("export CWL_URL={}\n".format(d["Job"]["App"]["cwl_url"]))
+            f_env.write("export MAIN_CWL={}\n".format(d["Job"]["App"]["main_cwl"]))
+            f_env.write("export CWL_FILES=\"{}\"\n".format(' '.join(d["Job"]["App"]["other_cwl_files"].split(','))))
         # other env variables
-        f_env.write("export OUTBUCKET={}\n".format(Dict["Job"]["Output"]["output_bucket_directory"]))
-        f_env.write("export PUBLIC_POSTRUN_JSON={}\n".format('1' if Dict["config"].get('public_postrun_json', False) else '0'))
+        f_env.write("export OUTBUCKET={}\n".format(d["Job"]["Output"]["output_bucket_directory"]))
+        f_env.write("export PUBLIC_POSTRUN_JSON={}\n".format('1' if d["config"].get('public_postrun_json', False) else '0'))
         env_preserv_str = ''
-        if "Env" in Dict["Job"]["Input"]:
-            for ev, val in Dict["Job"]["Input"]["Env"].iteritems():
+        if "Env" in d["Job"]["Input"]:
+            for ev, val in d["Job"]["Input"]["Env"].iteritems():
                 f_env.write("{}={}\n".format(ev, val))
                 env_preserv_str = env_preserv_str + "--preserve-environment " + ev + " "
         f_env.write("export PRESERVED_ENV_OPTION=\"{}\"\n".format(env_preserv_str))
