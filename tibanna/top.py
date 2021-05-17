320401e9a2342a4c5d46476c58aa09b1a6a55d61
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2021-05-17 12:26:31 -0400

    Remove double quotes from cmd

--- tibanna/top.py
+++ tibanna/top.py
@@ -231,7 +231,9 @@ class Top(object):
             last_minute = 5
         with open(csv_file, 'w') as fo:
             # header
-            fo.write(delimiter.join([colname_for_timestamps] + [Top.wrap_in_double_quotes(cmd) for cmd in self.commands]))
+            # we have to remove any double quotes that are present in the cmd, before wrapping it in double quotes. Otherwise we 
+            # will get incorrect column counts when creating the metrics report.
+            fo.write(delimiter.join([colname_for_timestamps] + [Top.wrap_in_double_quotes(cmd.replace('"', "'")) for cmd in self.commands]))
             fo.write('\n')
             # contents
             # skip timepoints earlier than timestamp_start
