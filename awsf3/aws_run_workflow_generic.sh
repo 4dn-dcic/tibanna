97bff3794e94cc6d5c44f04dbd8a3513e91b5a46
William Ronchetti <wrr33@cornell.edu>
2021-09-22 13:48:10 -0400

    repair syntax

--- awsf3/aws_run_workflow_generic.sh
+++ awsf3/aws_run_workflow_generic.sh
@@ -53,7 +53,7 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity| grep Account | sed 's/[^0-9
 # function that executes a command and collecting log
 exl(){ $@ >> $LOGFILE 2>> $LOGFILE; handle_error $?; } ## usage: exl command  ## ERRCODE has the error code for the command. if something is wrong, send error to s3.
 exlo(){ $@ 2>> /dev/null >> $LOGFILE; handle_error $?; } ## usage: exlo command  ## ERRCODE has the error code for the command. if something is wrong, send error to s3. This one eats stderr. Useful for hiding long errors or credentials.
-exl_no_error(){ $@ >> $LOGFILE 2>> $LOGFILE; ] ## same as exl but will not exit on error
+exl_no_error(){ $@ >> $LOGFILE 2>> $LOGFILE; } ## same as exl but will not exit on error
 
 # function that sends log to s3 (it requires LOGBUCKET to be defined, which is done by sourcing $ENV_FILE.)
 send_log(){  aws s3 cp $LOGFILE s3://$LOGBUCKET &>/dev/null; }  ## usage: send_log (no argument)
