eea4d712b102a86ae4687c8de20213ecd72c22b7
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2021-09-24 11:23:17 -0400

    Restore release script

--- release.sh
+++ release.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 # build and upload awsf docker image
-# export BUILD_LOG=/tmp/build-log
-# export VERSION=$(python -c 'from tibanna._version import __version__; print(__version__)')
-# export AWSF_IMAGE=$(python -c 'from tibanna.vars import DEFAULT_AWSF_IMAGE; print(DEFAULT_AWSF_IMAGE)')
-# docker build -t $AWSF_IMAGE --build-arg version=$VERSION awsf3-docker/ > $BUILD_LOG
-# docker push $AWSF_IMAGE
-
-
-# build and upload awsf docker image
 export BUILD_LOG=/tmp/build-log
 export VERSION=$(python -c 'from tibanna._version import __version__; print(__version__)')
-#export AWSF_IMAGE=$(python -c 'from tibanna.vars import DEFAULT_AWSF_IMAGE; print(DEFAULT_AWSF_IMAGE)')
-export AWSF_IMAGE="aveit/tibanna-awsf:${VERSION}"
-echo $AWSF_IMAGE
-docker build -t $AWSF_IMAGE --build-arg version=$VERSION awsf3-docker/ #> $BUILD_LOG
+export AWSF_IMAGE=$(python -c 'from tibanna.vars import DEFAULT_AWSF_IMAGE; print(DEFAULT_AWSF_IMAGE)')
+docker build -t $AWSF_IMAGE --build-arg version=$VERSION awsf3-docker/ > $BUILD_LOG
 docker push $AWSF_IMAGE
